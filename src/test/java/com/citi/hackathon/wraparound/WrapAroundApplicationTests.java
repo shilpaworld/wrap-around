95b6cdd2f15f558a2b34e5a01f09ef5625161905
Shilpa <shilpa.world@gmail.com>
2023-09-21 23:47:30 +0100

    initial commit

---
+++ src/test/java/com/citi/hackathon/wraparound/WrapAroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citi.hackathon.wraparound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrapAroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
